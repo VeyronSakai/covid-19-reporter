8782ef5183018750b665ec4355e3fd147d1e24d9
Yuki Sakai <yuukisakai123@gmail.com>
2022-01-26 05:26:49 +0900

    API叩いて東京都の感染者数を出力できるようにする

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
 use reqwest::*;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Deserialize, Debug)]
+struct ErrorInfo {
+    errorFlag: String,
+    errorCode: Option<String>,
+    errorMessage: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Item {
+    date: String,
+    name_jp: String,
+    npatients: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    errorInfo: ErrorInfo,
+    itemList: Vec<Item>,
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
+    // TODO: 日付を注入できるようにする
     let response = reqwest::get(
         "https://opendata.corona.go.jp/api/Covid19JapanAll?date=20220123&dataName=東京都",
     )
-    .await?;
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
 
-    let ret = response.text().await?;
+    let ret: Response = serde_json::from_str(&response).unwrap();
 
-    println!("{}", ret);
-
-    Ok(())
+    println!("{}", ret.itemList.first().unwrap().npatients);
 }
